2a9a64a884b568a915acb3dd24fdd67c9f9cbeec
Бобровский Дмитрий <ministrbob777@gmail.com>
2024-03-13 22:41:14 +0300

    save

--- Cryptorg/main.py
+++ Cryptorg/main.py
@@ -1,13 +1,17 @@
-from cryptorg import Api
+from cryptorg_futures import Api
 from settings import app_settings as appset
 
 
 def main():
 
+    # Cryptorg API instance
     cryptorg = Api(appset.api_key, appset.api_secret)
 
+    # Get list of all user's bots.
     bot_list = cryptorg.bot_list()
     print(bot_list)
+
+
 
 
 if __name__ == '__main__':
