550710f89cf1e1b1cf91799109adde7d4f9912b0
Dmitry Bobrovsky <ministrbob777@gmail.com>
2024-08-31 14:35:40 +0300

    add backup

--- backup.cmd
+++ backup.cmd
@@ -7,18 +7,18 @@ rem echo %date_name%
 rem Base Project Folder
 rem SET BASEPATH=c:\mysources
 rem Folder Project Name 
-SET PNAME=MyStudyProjects
+SET PNAME=TradingBots
 rem Backup Path
-SET BP=%BACKUP_PATH%\%PNAME%
+SET BP=%BACKUP_GIT_PATH%\%PNAME%
 rem Path to WinRAR
 rem SET WINRAR=C:\Program Files\WinRAR\WinRAR.exe
 rem Path to 7-Zip
-rem SET SZIP=c:\Program Files\7-Zip\7z.exe
+SET SZIP=c:\Program Files\7-Zip\7z.exe
 
 mkdir "%BP%"
 rem "%WINRAR%" a -r -s -m5 -md1024 -ag_YYYYMMDD-NN "%BP%\%PNAME%.rar" "c:\MyGit\%PNAME%\*"
-"%SZIP%" a -t7z -r -mx9 -mtc=on -mta=on -mtr=on -xr@exclude.txt -xr!__pycache__ "%BP%\%date_name%_%PNAME%.7z" "%BASEPATH%\%PNAME%\*"
-"%SZIP%" a -t7z -r -mx9 -mtc=on -mta=on -mtr=on -p%mypass% -ir@exclude.txt "%BP%\%date_name%_%PNAME%_PASS.7z" "%BASEPATH%\%PNAME%\exclude.txt"
+"%SZIP%" a -t7z -r -mx9 -mtc=on -mta=on -mtr=on -xr@exclude.txt -xr!__pycache__ "%BP%\%date_name%_%PNAME%.7z" "%MYGIT_PATH%\%PNAME%\*"
+"%SZIP%" a -t7z -r -mx9 -mtc=on -mta=on -mtr=on -p%mypass% -ir@exclude.txt "%BP%\%date_name%_%PNAME%_PASS.7z" "%MYGIT_PATH%\%PNAME%\exclude.txt"
 rem Delete backups except last 6
 cd /d "%BP%"
 for /f "skip=6 eol=: delims=" %%F in ('dir /b /o-d *.7z') do @del "%%F"
